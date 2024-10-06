d7834bdfdcd0dba1642873b35a678aacb1d6a806
afeef-c <afeefc123@gmail.com>
2024-10-06 12:59:13 +0530

    add req file

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -155,7 +155,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Directory for collected static files
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
